e606507b77efabdac44d672662bae5176f390755
KareeMagdyy <karim67899@gmail.com>
2021-06-14 08:09:00 +0200

    Ex:44

---
+++ Section 21 Functions Adv/func-adv.js
@@ -0,0 +1,29 @@
+// Coding Exercise 44:
+//----------------------
+const square = function(num){
+    let sum = num *= num;
+    return sum;
+}
+
+/**************************************************************/
+
+// Coding Exercise 45:
+//----------------------
+
+
+/**************************************************************/
+
+// Coding Exercise 46:
+//----------------------
+
+
+
+
+//**********************************/
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//**********************************/
