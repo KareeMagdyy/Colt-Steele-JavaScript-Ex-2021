53136616c604998e9d4ae1e5dbcc95308916c1a6
KareeMagdyy <karim67899@gmail.com>
2021-06-15 04:44:28 +0200

    EX:47~50

---
+++ Section 22 Callbacks & Array Methods/arrayMethods.js
@@ -0,0 +1,42 @@
+// Coding Exercise 47:
+//----------------------
+function cleanNames(array) {
+    return  array.map(function (n) {
+          return n.trim();
+      })
+  }
+
+/**************************************************************/
+
+// Coding Exercise 48:
+//----------------------
+const greet = (name) => {
+    return `Hey ${name}!`;
+} 
+
+/**************************************************************/
+
+// Coding Exercise 49:
+//----------------------
+function validUserNames(anArray) {
+    return anArray.filter(userName => userName.length < 10);
+}
+
+/**************************************************************/
+
+// Coding Exercise 50:
+//----------------------
+function allEvens(arr) {
+    return arr.every(x => x % 2 === 0);
+}
+
+
+
+//**********************************/
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//**********************************/
