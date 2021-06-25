70b5658805de0a92adf9890365d2bd0844aa95a7
KareeMagdyy <karim67899@gmail.com>
2021-06-25 05:56:20 +0200

    Ex 51~56

---
+++ Section 24 DOM/dom.js
@@ -0,0 +1,71 @@
+// Coding Exercise 51:
+//----------------------
+const image = document.getElementById('unicorn');
+const heading = document.getElementById('mainheading');
+
+/**************************************************************/
+
+// Coding Exercise 52:
+//----------------------
+const doneTodos = document.querySelectorAll('.done');
+const checkbox = document.querySelector('input[type="checkbox"]');
+
+/**************************************************************/
+
+// Coding Exercise 53:
+//----------------------
+const span1 = document.querySelector('span');
+span1.innerText = 'Disgusting';
+
+/**************************************************************/
+
+// Coding Exercise 54:
+//----------------------
+const eggImg = document.querySelector('#egg');
+
+eggImg.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+eggImg.alt = 'chicken';
+
+/**************************************************************/
+
+// Coding Exercise 55:
+//----------------------
+const container = document.querySelector('#container');
+const img = document.querySelector('img');
+
+container.style.textAlign = 'center';
+
+img.style.width = '150px';
+img.style.borderRadius = '50%';
+
+/**************************************************************/
+
+// Coding Exercise 56:
+//----------------------
+const sp = document.querySelectorAll('span');
+
+sp.forEach((sp, i) => {
+    sp.style.color = colors[i];
+});
+
+/**************************************************************/
+
+// Coding Exercise 57:
+//----------------------
+
+
+/**************************************************************/
+
+// Coding Exercise 58:
+//----------------------
+
+
+
+//**********************************/
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//**********************************/
