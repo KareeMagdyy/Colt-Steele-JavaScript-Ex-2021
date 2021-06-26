a20d1a9c368948b0a0f20c4b96f5ae78e0f51f5a
KareeMagdyy <karim67899@gmail.com>
2021-06-26 17:57:18 +0200

    Ex 59-60

---
+++ Section 25 DOM Events/events.js
@@ -0,0 +1,46 @@
+// Coding Exercise 59:
+//----------------------
+<body>
+    <h1 onclick="console.log('boo')">Inline Events Suck...</h1>
+    <button id="btn" onclick="console.log('clicked')">Click Me</button>
+</body>
+
+/**************************************************************/
+
+// Coding Exercise 60:
+//----------------------
+const helloBtn = document.querySelector('#hello');
+const byeBtn = document.querySelector('#goodbye');
+
+helloBtn.addEventListener('click', function (){
+    console.log('hello');
+});
+
+byeBtn.addEventListener('click', () => {
+    console.log('goodbye');
+});
+
+/**************************************************************/
+
+// Coding Exercise 61:
+//----------------------
+
+
+/**************************************************************/
+
+// Coding Exercise 62:
+//----------------------
+
+
+
+
+
+
+//**********************************/
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//Coded by Kareem Magdy El-Zomor ***
+//**********************************/
